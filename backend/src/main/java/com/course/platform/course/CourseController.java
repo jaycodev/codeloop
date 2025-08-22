c4452d04d8f53775cec2fbf6af5d65bf1aac02a9
Gonsalo Gonzales <andygp06002@gmail.com>
2025-08-22 14:07:28 -0500

    "Add method to insert courses"

--- backend/src/main/java/com/course/platform/course/CourseController.java
+++ backend/src/main/java/com/course/platform/course/CourseController.java
@@ -5,8 +5,14 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.course.platform.course.dto.CourseCreateDTO;
+import com.course.platform.user.User;
+import com.course.platform.user.UserService;
 
 import lombok.RequiredArgsConstructor;
 
@@ -18,6 +24,9 @@ public class CourseController {
 	@Autowired
 	private final CourseService coursesServices;
 	
+	@Autowired
+	private final UserService userServices;
+	
 	@GetMapping("/list")
 	public List<Course> list(){
 		return coursesServices.listCours();
@@ -27,6 +36,20 @@ public class CourseController {
 	public Course search(@PathVariable("id") Integer id) {
 		return coursesServices.search(id);
 	}
+	
+	@PostMapping("/create")
+	public Course create(@RequestBody CourseCreateDTO courseDto) {
+		User teacher = userServices.search(courseDto.getTeacherId());
+	    Course course = Course.builder()
+	            .title(courseDto.getTitle())
+	            .description(courseDto.getDescription())
+	            .price(courseDto.getPrice())
+	            .teacher(teacher)
+	            .build();
+
+	    return coursesServices.keep(course);
+	}
+
 	
 	/*@GetMapping("/find/courses/{id}")
 	public List<CourseDTO> listCourseByTeacher(@PathVariable("teacherid") Integer teacherid) {
