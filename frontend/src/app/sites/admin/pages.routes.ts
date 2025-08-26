cc557d98b856419980cf5f83e09678ffca90df7e
jason-vila <jasonvilac@gmail.com>
2025-08-26 13:09:01 -0500

    ♻️ refactor: resolve conflicts for main merge

--- frontend/src/app/sites/admin/pages.routes.ts
+++ frontend/src/app/sites/admin/pages.routes.ts
@@ -1,11 +1,12 @@
 import { Routes } from '@angular/router';
-import { EnrollmentCrud } from './cruds/enrollment/enrollment-crud';
+import { EnrollmentCrud } from './cruds/enrollment/enrollment.crud';
 import { ExamCrud } from './cruds/exam/exam-crud';
-import { CourseCrud } from './courses/pages/course-crud';
+import { CourseCrud } from './cruds/course/course-crud';
 import { Crud } from './cruds/CRUD-EXAMPLE/crud';
-import { QuestionCrud } from './cruds/questions/components/question-crud';
-import { AnswerListByQuestion } from './cruds/answers/components/answer-by-question';
-import { AnswerList } from './cruds/answers/components/answer-list';
+// import { QuestionCrud } from './questions/components/question-crud';
+import { AnswerListByQuestion } from './answers/components/answer-by-question';
+import { AnswerList } from './answers/components/answer-list';
+import { UserCrudComponent } from './cruds/users/users';
 
 export default [
   { path: 'answer', component: AnswerList },
@@ -15,7 +16,7 @@ export default [
   { path: 'exam', component: ExamCrud },
   { path: 'lesson', component: Crud },
   { path: 'payment', component: Crud },
-  { path: 'question', component: QuestionCrud },
-  { path: 'user', component: Crud },
+  // { path: 'question', component: QuestionCrud },
+  { path: 'user', component: UserCrudComponent },
   { path: '**', redirectTo: '/notfound' },
 ] as Routes;
