9d029531c656c925a3f42d338a398443f45c1a8c
Juan <juanjoseblas344@gmail.com>
2025-08-26 06:11:16 -0500

    ♻️ refactor(routes): update admin routes for new components

--- frontend/src/app/sites/admin/pages.routes.ts
+++ frontend/src/app/sites/admin/pages.routes.ts
@@ -3,15 +3,19 @@ import { EnrollmentCrud } from './cruds/enrollment/enrollment-crud';
 import { ExamCrud } from './cruds/exam/exam-crud';
 import { CourseCrud } from './courses/pages/course-crud';
 import { Crud } from './cruds/CRUD-EXAMPLE/crud';
+import { QuestionCrud } from './questions/components/question-crud';
+import { AnswerListByQuestion } from './answers/components/answer-by-question';
+import { AnswerList } from './answers/components/answer-list';
 
 export default [
-  { path: 'answer', component: EnrollmentCrud },
+  { path: 'answer', component: AnswerList },
+  { path: 'answers/question/:questionId', component: AnswerListByQuestion },
   { path: 'course', component: CourseCrud },
   { path: 'enrollment', component: EnrollmentCrud },
   { path: 'exam', component: ExamCrud },
   { path: 'lesson', component: Crud },
   { path: 'payment', component: Crud },
-  { path: 'question', component: Crud },
+  { path: 'question', component: QuestionCrud },
   { path: 'user', component: Crud },
   { path: '**', redirectTo: '/notfound' },
 ] as Routes;
